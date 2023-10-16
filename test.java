7f1fd3f1fa097e99841a09416ce71a215e606a2a
KT <thun.kristian@gmail.com>
2023-10-16 12:00:55 +0200

    Added new stuff

--- test.java
+++ test.java
@@ -3,17 +3,24 @@ public class test {
 
 
     // Ändrade lite till. Igen Ändrar jag en kommentar
-    public void test(){
+    public void test() {
         // Det här är en funktion. VAd händer om det ligger en annan funktion här. Är det så smart?
     }
-    
+
 
     int b = 3;
-    // Ändrade lite till. 
-    public void test2(){
+
+    // Ändrade lite till.
+    public void test2() {
         int a = 2;
-        
+
         // Det här är en funktion som gör något annat
+
+    }
+
+    public void addingAnotherFunctionInABranch(){
+        // It does something sweet.
+    }
 
 }
     
