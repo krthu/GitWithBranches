07e2f149b970939194001d7f4b2721e9667baa6e
KT <thun.kristian@gmail.com>
2023-10-16 11:47:15 +0200

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,10 @@
 public class test {
     // Det här är lite större ändringar.
-
+    int b = 3;
     // Ändrade lite till. 
-    
+    public void test2(){
+        int a = 2;
+        
+        // Det här är en funktion som gör något annat
 }
+    
