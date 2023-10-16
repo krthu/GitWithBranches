d97aa72214ff8df623153476eaab96baf14e9c4b
KT <thun.kristian@gmail.com>
2023-10-16 11:47:44 +0200

    Changed some stuff.

--- test.java
+++ test.java
@@ -1,6 +1,9 @@
 public class test {
     // Det här är lite större ändringar.
 
-    // Ändrade lite till. 
+    // Ändrade lite till. Igen Ändrar jag en kommentar
+    public void test(){
+        // Det här är en funktion. VAd händer om det ligger en annan funktion här. Är det så smart?
+    }
     
 }
